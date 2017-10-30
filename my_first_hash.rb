5b635c23ac77e1260fe0fdd14193cae0a0a40df4
Eric McKinney <mckinney99@gmail.com>
2017-10-30 14:58:38 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,7 +28,7 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
+  shipping_manifest ["hey1"] = "value1"
   #your code here
   #remember to return the shipping_manifest hash
 
